e42768778b4229c68cd9b9e544d4c01e68a7b25d
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-10-03 11:52:06 +0300

    Create phoneList component

---
+++ src/Components/PhoneList/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function liPhones() {
+  const arrayPhonesNumbers = ["85468", "85748", "45485", "44858", "12348"];
+  const liPhonesNumbers = arrayPhonesNumbers.map((number) => {
+    return <li key = {number}>{number}</li>;
+  });
+
+  return (
+    <div>
+      <h4>Список контактов</h4>
+      <ul>{liPhonesNumbers}</ul>
+    </div>
+  );
+}
